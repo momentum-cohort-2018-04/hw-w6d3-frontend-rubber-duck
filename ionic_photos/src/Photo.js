7576087123a9a4ac08a08f586fdedcce0e7f23af
MiniRaccoonDog <atorres@spoonflower.com>
2018-05-31 22:28:58 -0400

    modal working! committing

--- ionic_photos/src/Photo.js
+++ ionic_photos/src/Photo.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 class Photo extends Component {
+  render () {
+    console.log('????????')
+    const origArray = this.props.array
+    const newArray = origArray.map(function (entry, id) {
+      return (
+
+        <PhotoModal index={id} entry={entry} array={origArray} key={id} />)
+    })
+    return newArray
+  }
+}
+
+class PhotoModal extends Component {
   constructor () {
     super()
     this.state = {
@@ -13,37 +26,10 @@ class Photo extends Component {
     this.setState({open: boolie})
   }
   render () {
-    const modalState = this.state.open
-    const modalToggle = this.toggleModal.bind(this)
-    const origArray = this.props.array
-    const newArray = origArray.map(function (entry, id) {
-      let thumbnail = entry.urls.thumb
-      let description
-      if (entry.description) {
-        description = entry.description
-      } else {
-        description = 'No Description'
-      }
-      return (
-        <div className='level-item' key={id}>
-          <figure className='image is-128x128'>
-            <img src={thumbnail} alt={description} onClick={modalToggle} />
-          </figure>
-          <PhotoModal index={id} array={origArray} toggle={modalToggle} open={modalState} />
-        </div>)
-    })
-    return newArray
-  }
-}
-
-class PhotoModal extends Component {
-  // index={id} array={origArray} onClick={modalToggle} open={modalState}
-
-  render () {
-    console.log(this.props.open)
-    console.log(this.props.array[this.props.index])
-    console.log(this.props.index)
-    const entry = this.props.array[this.props.index]
+    console.log('WELLLL???!?!?')
+    let id = this.props.index
+    const entry = this.props.array[id]
+    let thumbnail = entry.urls.thumb
     let description
     if (entry.description) {
       description = entry.description
@@ -63,26 +49,36 @@ class PhotoModal extends Component {
     if (entry.location) {
       photoLocate = entry.location.title
     }
-    if (this.props.open) {
+    if (this.state.open) {
       return (
-        <div className='modal is-active'>
-          <div className='modal-background' />
-          <div className='modal-content'>
-            <p className='image'>
-              <img src={regularImage} id={unsplashID} alt={description} />
-            </p>
-            <ul>
-              <li>Photographed by {photog}</li>
-              <li><small>{photogUN}</small></li>
-              <li><a href={portfolio}>View Portfolio</a></li>
-              {photoLocate && <li>Taken in {photoLocate}</li>}
-            </ul>
+        <div className='level-item'>
+          <figure className='image is-128x128'>
+            <img src={thumbnail} alt={description} onClick={this.toggleModal} />
+          </figure>
+          <div className='modal is-active'>
+            <div className='modal-background' />
+            <div className='modal-content'>
+              <p className='image'>
+                <img src={regularImage} id={unsplashID} alt={description} />
+              </p>
+              <ul>
+                <li>Photographed by {photog}</li>
+                <li><small>{photogUN}</small></li>
+                <li><a href={portfolio}>View Portfolio</a></li>
+                {photoLocate && <li>Taken in {photoLocate}</li>}
+              </ul>
+            </div>
+            <button className='modal-close is-large' aria-label='close' onClick={this.toggleModal} />
           </div>
-          <button className='modal-close is-large' aria-label='close' onClick={this.props.toggle} />
-        </div>
-      )
+        </div>)
     } else {
-      return (<div className='modal' />)
+      console.log('RETURN LIKE A BASIC BITCH')
+      return (
+        <div className='level-item'>
+          <figure className='image is-128x128'>
+            <img src={thumbnail} alt={description} onClick={this.toggleModal} />
+          </figure>
+        </div>)
     }
   }
 }
